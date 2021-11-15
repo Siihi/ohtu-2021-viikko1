30d815fd4cb5fa70676d08d696405a01c392e5af
Siiri <siiri.i.hirvonen@gmail.com>
2021-11-15 15:28:48 +0200

    viikko 2 korjattu

--- src/varasto.py
+++ src/varasto.py
@@ -1,7 +1,8 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
         self.tilavuus = 0.0
-        self.tilavuus = tilavuus
+        if tilavuus > 0.0:
+            self.tilavuus = tilavuus
 
         if alku_saldo < 0.0:
             # virheellinen, nollataan
